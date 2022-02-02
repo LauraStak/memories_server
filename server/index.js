2f3ddc0c4b066a68d51696713a7bdeaf9917ad47
LauraStak <laura.stakyte@gmail.com>
2022-02-02 21:05:12 +0200

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import postRoute from "./routes/posts.js";
+
+const app = express();
+dotenv.config();
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+
+app.use(cors());
+
+app.use("/posts", postRoute);
+
+mongoose.connect(process.env.CONNECT_URL);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("listening on port 5000");
+});
